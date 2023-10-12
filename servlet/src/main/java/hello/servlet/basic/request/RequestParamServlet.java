113fae82e793565d356c543b5c32ec0bfb835e79
SPIDEY965 <bbnt25@gmail.com>
2023-10-12 15:16:51 +0900

    learn : spring-http-req-res

--- servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
+++ servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -7,15 +7,39 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+/*
+    1. 파라미터 전송 기능
 
-// 1. 파라미터 전송 기능
-// http://localhost:8080/request-param?username=hello&age=20
+ http://localhost:8080/request-param?username=hello&age=20
+*
+*/
 //
 
 @WebServlet(name = "requestParamServlet", urlPatterns = "/request-param")
 public class RequestParamServlet extends HttpServlet {
     @Override
-    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.service(req, resp);
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("[전체 파라미터 조회] - start");
+
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + "=" + request.getParameter(paramName)));
+        System.out.println("[전체 파라미터 조회] - end");
+
+        System.out.println("[단일 파라미터 조회]");
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+        System.out.println();
+
+        // http://localhost:8080/request-param?username=hello&age=20&username=hi&username=Bonjour
+        System.out.println("[이름이 같은 복수 파라미터 조회]");
+        // request.getParameter("username") -> hello 가 나온다(맨처음것)
+        // 중복으로 보내는 경우는 거의 없다
+        String[] usernames = request.getParameterValues("username");
+        for (String name : usernames) {
+            System.out.println("name = " + name);
+        }
     }
 }
